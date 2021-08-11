f152317bca8ecbd1ea157c0311a57251b5c15f0b
Rachmizard <rachmizard11072000@gmail.com>
2021-08-12 01:56:29 +0700

    add: structure doctor page component

---
+++ src/components/molecules/HomeProfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+export default function HomeProfile() {
+  return (
+    <View>
+      <Image />
+      <Text>Home Profile</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
