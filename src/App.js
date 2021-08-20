07aa6d3d035911cc6e0004ac69faabdd197045d5
Rachmizard <rachmizard11072000@gmail.com>
2021-08-20 20:08:22 +0700

    add: initialize graphql setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import Navigations from 'navigations';
 import {usePersistNavigation} from 'hooks';
 import {ActivityIndicator} from 'react-native';
+import {ApolloProvider} from '@apollo/client';
+import {client} from 'gql';
 
 export default function App() {
   const {initialState, getPersistenceKey, isReady, setAsyncStorage} =
@@ -13,12 +15,14 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer
-      initialState={initialState}
-      onStateChange={state =>
-        setAsyncStorage(getPersistenceKey, JSON.stringify(state))
-      }>
-      <Navigations />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer
+        initialState={initialState}
+        onStateChange={state =>
+          setAsyncStorage(getPersistenceKey, JSON.stringify(state))
+        }>
+        <Navigations />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
