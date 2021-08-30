1b92679197a31b9434e05585e9adf7e06e03fb47
Rachmizard <rachmizard11072000@gmail.com>
2021-08-30 12:40:22 +0700

    remove(reducer): unused user reducer

--- src/stores/rootReducers.js
+++ src/stores/rootReducers.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux';
 import authReducer from './auth/auth.reducer';
-import userReducer from './user/user.reducer';
 
 const rootReducers = combineReducers({
-  userReducer: userReducer,
   authReducer: authReducer,
 });
 
--- src/stores/user/user.reducer.js
+++
@@ -1,9 +0,0 @@
-const INITIAL_STATE = {
-  user: {},
-};
-
-const userReducer = (state = INITIAL_STATE, action) => {
-  return state;
-};
-
-export default userReducer;
