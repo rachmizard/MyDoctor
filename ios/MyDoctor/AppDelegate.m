922129c7812deeb95126fae6e53aa116003b334e
Rachmizard <rachmizard11072000@gmail.com>
2021-10-12 11:57:48 +0700

    update(): IOS pod install & fix unrecognized fonts

--- ios/MyDoctor/AppDelegate.m
+++ ios/MyDoctor/AppDelegate.m
@@ -27,6 +27,17 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+
+  for (NSString* family in [UIFont familyNames])
+  {
+      NSLog(@"%@", family);
+
+      for (NSString* name in [UIFont fontNamesForFamilyName: family])
+      {
+          NSLog(@"Family name:  %@", name);
+      }
+  }
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
